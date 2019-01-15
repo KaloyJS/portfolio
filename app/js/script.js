da187c6d41bed82f9e664cb033debe31c46bc39a
Kaloy <kaloywebdev@gmail.com>
2019-01-14 19:46:04 -0500

    Completed the technical skills section.

--- app/js/script.js
+++ app/js/script.js
@@ -6,10 +6,55 @@ $(document).ready(function() {
 		pagination: false
 	});
 
+	//Typed settings
 	var typed = new Typed(".typed", {
 		strings: ["Web Developer." , "coder.", "seeker.", ],
 		typeSpeed: 140,
 		loop: true,
 		startDelay: 1000
 	});
+
+	//Owl carousel settings
+	$('.owl-carousel').owlCarousel({
+    loop:true,
+    items: 4,
+    responsive:{
+	        0:{
+	            items:1
+	        },
+	        480:{
+	            items:2
+	        },
+	        768:{
+	            items:3
+	        },
+	        938:{
+	        	items:4
+	        }
+	    }
+	});
+
+	
+
+
+      var skillsTopOffset = $(".skillsSection").offset().top;
+
+
+      $(window).scroll(function() {
+
+      		if(window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
+      				//easy pie chart settings
+				    $('.chart').easyPieChart({
+			            easing: 'easeInOut',
+			            barColor: '#fff',
+			            trackColor: false,
+			            scaleColor: false,
+			            lineWidth:  4,
+			            size: 152,
+			            onStep: function(from, to, percent) {
+			            	$(this.el).find('.percent').text(Math.round(percent));
+			            }
+			        });
+      		}
+      });
 });
